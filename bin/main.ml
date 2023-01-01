f5caf6a0c78aaae41ba71772c5d46c780186a51f
Christiano Haesbaert <haesbaert@haesbaert.org>
2023-01-01 01:40:12 +0100

    almost there

--- bin/main.ml
+++ bin/main.ml
@@ -5,42 +5,63 @@ let _xspf_header =
 	<trackList>
 |}
 
-let traverse path =
-  let queue = Queue.create () in
+let consider_file path =
+  match Av.open_input path with
+  | exception (Avutil.Error e) ->
+    Printf.eprintf "%s: error %s%!\n" path (Avutil.string_of_error e)
+  | handle ->
+    (match Av.get_input_duration handle with
+     | Some duration ->
+       Printf.printf "%s: duration %Lu\n%!" path duration
+     | None ->
+       Printf.eprintf "%s: NO DURATION !!!\n%!" path;
+       Av.close handle)
+
+let traverse path queue =
   let rec loop path =
-    let typ = Unix.LargeFile.stat path in
-    match typ.st_kind with
+    match (Unix.LargeFile.stat path).st_kind with
     | S_REG -> Queue.add path queue
     | S_DIR ->
-      let () = 
-        match Sys.readdir path with
-        | children ->
-          Array.fast_sort String.compare children;
-          Array.iter (fun child -> loop (Printf.sprintf "%s/%s" path child)) children
-        | exception (Sys_error e) -> Printf.eprintf "%s%!\n" e
-      in ()
+      (match Sys.readdir path with
+       | children ->
+         Array.fast_sort String.compare children;
+         Array.iter (fun child -> loop (Printf.sprintf "%s/%s" path child)) children
+       | exception (Sys_error e) -> Printf.eprintf "%s%!\n" e)
     | _ -> ()
   in
-  loop path;
-  queue
+  loop path
 
 let rec dump_queue oc queue =
   match Queue.take_opt queue with
   | None -> ()
-  | Some _path ->
+  | Some path ->
+    consider_file path;
     (* Printf.fprintf oc "%s\n%!" path; *)
     dump_queue oc queue
 
-let xspfmaker oc =
-  dump_queue oc (traverse "/etc")
-  (* Out_channel.output_string oc xspf_header *)
+let paths_of_filelist ic =
+  let rec loop l =
+    match input_line ic with
+    | path -> loop @@ path :: l
+    | exception End_of_file -> l
+  in
+  loop [] |> List.rev
   
-let main outputfile =
+let xspfmaker oc paths =
+  let queue = Queue.create () in
+  List.iter (fun path -> traverse path queue) paths;
+  dump_queue oc queue
+
+let main outputfile paths =
   let outputfile = match outputfile with
     | "" -> stdout
     | outputfile -> open_out outputfile
   in
-  xspfmaker outputfile;
+  let paths = match paths with
+    | "-" :: [] | [] -> paths_of_filelist stdin
+    | paths -> paths
+  in
+  xspfmaker outputfile paths;
   if outputfile <> stdout then
     close_out_noerr outputfile
 
@@ -51,9 +72,12 @@ let () =
       Arg.(value & opt string "" & info ["o" ; "output"]
              ~doc:"Output file, stdout if not specified.")
     in
-    
+    let paths =
+      Arg.(value & pos_all string [] & info []
+             ~doc:"Sei la caraleo.")
+    in
     Cmd.v
       (Cmd.info "xspfmaker" ~doc:"xspfmaker whatever")
-      Term.(const main $ outputfile)
+      Term.(const main $ outputfile $ paths)
   in
   exit @@ Cmd.eval cmd
