bff8eaf2d889c2527224f28e8160988a592b9d3d
Christiano Haesbaert <haesbaert@haesbaert.org>
2023-01-07 21:35:25 +0100

    Finalize!? manpage

--- bin/xspfmaker.ml
+++ bin/xspfmaker.ml
@@ -30,26 +30,57 @@ let preprocess title_fmt input output paths =
 let () =
   let open Cmdliner in
   let cmd =
+    let examples =
+      let esc = Manpage.escape in
+      [ `S Manpage.s_examples;
+        `P "Create a xspf list from two directories a single file:"; `Noblank;
+        `P  ("$ xspfmaker ~/videos ~/nudes.mp4 /mnt/oldvids > playlist.xspf" |> esc);
+        `P "or the equivalent:"; `Noblank;
+        `P ("$ xspfmaker -o playlist.xspf ~/videos ~/nudes.mp4 /mnt/oldvids" |> esc);
+        `P "or the equivalent getting the list from stdin:"; `Noblank;
+        `P ({|$ printf "~/videos\n~/nudes.mp4\n/mnt/oldvids" | xspfmaker -o playlist.xspf|} |> esc);
+        `P "or the equivalent but parsing the list from a file:"; `Noblank;
+        `P ({|$ printf "~/videos\n~/nudes.mp4\n/mnt/oldvids" > list &&|} |> esc); `Noblank;
+        `P ("xspfmaker -o playlist.xspf -i list" |> esc);
+      ]
+    in
+    let doc = "Display the last part of a file" in
+    let man = [
+      `S Manpage.s_description;
+      `P "$(tname) prints the last lines of each $(i,FILE) to standard output. If
+        no file is specified reads standard input. The number of printed
+        lines can be  specified with the $(b,-n) option.";
+      `S Manpage.s_exit_status;
+      `P "$(b,xspfmaker) exits 0 on success, and > 0 if an error occurs.";
+      `Blocks examples;
+      `S Manpage.s_bugs;
+      `P "https://github.com/haesbaert/xspfmaker/issues";
+      `S Manpage.s_see_also;
+      `P "$(b,find)(1), $(b,vlc)(1)";
+      `S Manpage.s_authors;
+      `P "Christiano Haesbaert <haesbaert@haesbaert.org>";
+    ]
+    in
     let title_fmt =
       let doc =
         "Specifies the format of the title of each track, $(docv) must be either $(b,path) \
          or $(b,filename). If $(b,path) the title is the complete file path. If $(b,filename) \
-         just the actual filename is used as the title."
+         just the actual filename is used as the title. If empty or $(b,-), read a list of one \
+         path per line from stdin."
       and docv = "FMT" in
       let t = Arg.enum [("path", Path); ("filename", Filename)] in
       Arg.(value & opt t Filename & info ["t"; "title-format"] ~doc ~docv)
     in
     let input =
-      let doc =
-        "input"
+      let doc = "Input file with a list of one path per line to be to be used in addition \
+                 to $(i,PATHS)."
       and docv = "INPUTFILE" in
-      Arg.(value & opt (some string) None & info ["-i"; "input-file"] ~doc ~docv)
+      Arg.(value & opt (some string) None & info ["i"; "input-file"] ~doc ~docv)
     in
     let output =
-      let doc =
-        "output"
+      let doc = "Output file to be used instead of stdout."
       and docv = "OUTPUTFILE" in
-      Arg.(value & opt (some string) None & info ["-o"; "output-file"] ~doc ~docv)
+      Arg.(value & opt (some string) None & info ["o"; "output-file"] ~doc ~docv)
     in
     let paths =
       let doc =
@@ -59,7 +90,7 @@ let () =
       Arg.(value & pos_all string [] & info [] ~doc ~docv)
     in
     Cmd.v
-      (Cmd.info "xspfmaker" ~doc:"Make xspf playlists")
+      (Cmd.info "xspfmaker" ~doc ~man ~exits:[])
       Term.(const preprocess $ title_fmt $ input $ output $ paths)
   in
   exit @@ Cmd.eval cmd
